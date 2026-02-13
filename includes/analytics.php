7e83fe8a90dea874917c248bde6d3c0d41b53687
Mesud karakayalı <msdkarakayali@gmail.com>
2026-02-13 17:46:28 +0300

    d

--- includes/analytics.php
+++ includes/analytics.php
@@ -1,4 +1,10 @@
 <!-- Google tag (gtag.js) -->
+<link rel="icon" type="image/x-icon" href="/favicon.ico">
+<link rel="shortcut icon" href="/favicon.ico">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png">
+<link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png">
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-HX90LVYB1B"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
